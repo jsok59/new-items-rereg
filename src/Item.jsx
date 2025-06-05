7d3a4e1c5635030651b65093a621fc0964a70cb0
John <jsok@rheebros.com>
2025-06-05 16:11:44 -0400

    Add fields to Item component

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
 import { loadCsvAsJson } from "./data.jsx";
 
-export default function Item({ itemNum, brand, description, kor_description }) {
-	return (
-		<div className="Item">
-			<div className="header">
-				<div className="itemnum">{itemNum}</div>
-				<div className="kor-description">{kor_description}</div>
-			</div>
-			<div className="content">
-				<div className="top">
-					<div className="left">
-						<div className="description">
-							{brand}, {description}
-						</div>
-					</div>
-					<img src={"../public/" + itemNum + ".jpg"} alt="item" />
-				</div>
-			</div>
-		</div>
-	);
+export default function Item({
+  itemNum,
+  brand,
+  description,
+  kor_description,
+  uom,
+  size,
+  exd,
+}) {
+  return (
+    <div className="Item">
+      <div className="header">
+        <div className="itemnum">{itemNum}</div>
+        <div className="kor-description">{kor_description}</div>
+      </div>
+      <div className="content">
+        <div className="top">
+          <div className="left">
+            <div className="description">
+              {brand}, {description}
+            </div>
+            <div className="size">{size}</div>
+            <div className="flex-container">
+              <div className="uom">{uom}</div>
+              <div className="exd">{exd}</div>
+            </div>
+          </div>
+          <img src={"../public/" + itemNum + ".jpg"} alt="item" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/NewItems.jsx
+++ src/NewItems.jsx
@@ -4,80 +4,158 @@ import Item from "./Item.jsx";
 import { loadCsvAsJson } from "./data.jsx";
 
 export default function NewItems() {
-	const [items, setItems] = useState([
-		{ id: "00020D", barcode: "Rice" },
-		{ id: "00030D", barcode: "Rice2" },
-	]);
+  const [items, setItems] = useState([
+    { id: "00020D", barcode: "Rice" },
+    { id: "00030D", barcode: "Rice2" },
+  ]);
 
-	const [isOpen, toggleModal] = useState(false);
-	const [data, setData] = useState([]);
+  const [isOpen, toggleModal] = useState(false);
+  const [data, setData] = useState([]);
 
-	useEffect(() => {
-		loadCsvAsJson().then((json) => setData(json));
-	}, []);
+  useEffect(() => {
+    loadCsvAsJson().then((json) => setData(json));
+  }, []);
 
-	const openModal = () => toggleModal(true);
-	const closeModal = () => toggleModal(false);
+  const openModal = () => toggleModal(true);
+  const closeModal = () => toggleModal(false);
 
-	if (items.length > 5) {
-		setItems(null);
-	}
+  if (items.length > 5) {
+    setItems(null);
+  }
 
-	return (
-		<div className="NewItems">
-			<div>
-				<h3>New Items</h3>
-				<button onClick={openModal}>Add Item</button>
-			</div>
-			<NewItemModal closeModal={closeModal} isOpen={isOpen} data={data}></NewItemModal>
+  return (
+    <div className="NewItems">
+      <div>
+        <h3>New Items</h3>
+        <button onClick={openModal}>Add Item</button>
+      </div>
+      <NewItemModal
+        closeModal={closeModal}
+        isOpen={isOpen}
+        data={data}
+      ></NewItemModal>
 
-			<ul>
-				{items.map((item) => (
-					<li key={item.id}>
-						<p>{item.id}</p>
-						<p>{item.name}</p>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <p>{item.id}</p>
+            <p>{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function NewItemModal({ closeModal, isOpen, data }) {
-	const [itemNum, setItemNum] = useState("");
-	const [description, setDescription] = useState("");
-	const [brand, setBrand] = useState("");
-	const [kor_description, setKorDescription] = useState("");
+  const [itemNum, setItemNum] = useState("");
+  const [description, setDescription] = useState("");
+  const [brand, setBrand] = useState("");
+  const [kor_description, setKorDescription] = useState("");
+  const [uom, setUom] = useState("");
+  const [size, setSize] = useState("");
+  const [exd, setExd] = useState("");
 
-	function handleItemNum(e) {
-		setItemNum(e.target.value);
-	}
+  function handleItemNum(e) {
+    setItemNum(e.target.value);
+  }
 
-	useEffect(() => {
-		const item = data.find((obj) => obj["No."] === itemNum);
-		console.log(item);
-		if (item) {
-			setDescription(item["Description"]);
-			setBrand(item["Brand"]);
-			setKorDescription(item["Description 2"]);
-		}
-	}, [itemNum, data]);
-	return (
-		<div className={isOpen ? "NewItemModal" : "NewItemModal hidden"}>
-			<form action="">
-				<label for="itemNum">Item Number:</label>
-				<input type="text" id="itemNum" name="itemNum" onChange={handleItemNum} />
-				<label for="itemNum">Brand:</label>
-				<input type="text" id="brand" name="brand" value={brand} onChange={(e) => setBrand(e.target.value)} />
-				<label for="itemNum">Korean Description:</label>
-				<input type="text" id="kor-description" name="kor-description" value={kor_description} onChange={(e) => setKorDescription(e.target.value)} />
-				<label for="itemNum">Description:</label>
-				<input type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
-				<button onClick={closeModal}>Done</button>
-			</form>
-			<div className="preview-container">
-				<Item itemNum={itemNum} brand={brand} description={description} kor_description={kor_description}></Item>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    const item = data.find((obj) => obj["No."] === itemNum);
+    console.log(item);
+    if (item) {
+      setDescription(item["Description"]);
+      setBrand(item["Brand"]);
+      setKorDescription(item["Description 2"]);
+      setUom(item["Base Unit of Measure"]);
+      setSize(item["Size Description"]);
+    }
+  }, [itemNum, data]);
+  return (
+    <div className={isOpen ? "NewItemModal" : "NewItemModal hidden"}>
+      <form action="">
+        <div>
+          <label for="itemNum">Item Number:</label>
+          <input
+            type="text"
+            id="itemNum"
+            name="itemNum"
+            onChange={handleItemNum}
+          />
+        </div>
+        <div>
+          <label for="itemNum">Brand:</label>
+          <input
+            type="text"
+            id="brand"
+            name="brand"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+        </div>
+        <div>
+          <label for="itemNum">Korean Description:</label>
+          <input
+            type="text"
+            id="kor-description"
+            name="kor-description"
+            value={kor_description}
+            onChange={(e) => setKorDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label for="itemNum">Description:</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label for="itemNum">Size:</label>
+          <input
+            type="text"
+            id="size"
+            name="size"
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+          />
+        </div>
+        <div>
+          <label for="itemNum">Base Uom:</label>
+          <input
+            type="text"
+            id="uom"
+            name="uom"
+            value={uom}
+            onChange={(e) => setUom(e.target.value)}
+          />
+        </div>
+        <div>
+          <label for="itemNum">ExD:</label>
+          <input
+            type="text"
+            id="exd"
+            name="exd"
+            value={exd}
+            onChange={(e) => setExd(e.target.value)}
+          />
+        </div>
+        <button onClick={closeModal}>Done</button>
+      </form>
+      <div className="preview-container">
+        <Item
+          itemNum={itemNum}
+          brand={brand}
+          description={description}
+          kor_description={kor_description}
+          uom={uom}
+          size={size}
+          exd={exd}
+        ></Item>
+      </div>
+    </div>
+  );
 }
