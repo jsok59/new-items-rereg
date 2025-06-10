68a51a6127dbf14e2b33af745f23c37458cfc5c2
John Sok <98001557+jsok59@users.noreply.github.com>
2025-06-10 15:57:38 -0400

    Added remove button and display item content

--- src/Item.jsx
+++ src/Item.jsx
@@ -3,74 +3,58 @@ import { loadCsvAsJson } from "./data.jsx";
 import JsBarcode from "jsbarcode";
 import "./Item.css";
 
-export default function Item({
-  itemNum,
-  brand,
-  description,
-  kor_description,
-  uom,
-  size,
-  exd,
-  priceCode,
-  barcodeRetail,
-  barcodeNotRetail,
-  category,
-}) {
-  function Barcode({ value = "" }) {
-    const svgRef = useRef();
+export default function Item({ itemNum, brand, description, kor_description, uom, size, exd, priceCode, barcodeRetail, barcodeNotRetail, category, editItem }) {
+	function Barcode({ value = "" }) {
+		const svgRef = useRef();
 
-    useEffect(() => {
-      if (value) {
-        JsBarcode(svgRef.current, value, {
-          format: "CODE128",
-          lineColor: "#000",
-          width: 1.2,
-          height: 15,
-          displayValue: true,
-          fontSize: 10,
-          margin: 1,
-        });
-      }
-    }, [value]);
+		useEffect(() => {
+			if (value) {
+				JsBarcode(svgRef.current, value, {
+					format: "CODE128",
+					lineColor: "#000",
+					width: 1.2,
+					height: 15,
+					displayValue: true,
+					fontSize: 10,
+					margin: 1,
+				});
+			}
+		}, [value]);
 
-    return <svg ref={svgRef}></svg>;
-  }
-  return (
-    <div className="Item">
-      <div className="header">
-        <div className="itemnum">{itemNum}</div>
-        <div className="kor-description">{kor_description}</div>
-      </div>
-      <div className="content">
-        <div className="left">
-          <div className="container-1">
-            <div className="description">
-              {brand}, {description}
-            </div>
-          </div>
-          <div className="container-2">
-            <div className="size">{size}</div>
-            <div className="flex-container">
-              <div className="uom">{uom}</div>
-              <div className="exd">{exd}</div>
-              <div className="priceCode">{priceCode}</div>
-            </div>
-            <div className="barcodeRetail">{barcodeRetail}</div>
-          </div>
-          <div className="container-3">
-            <div className="barcodeNotRetail">{barcodeNotRetail}</div>
-            <Barcode value={barcodeRetail}></Barcode>
-          </div>
-        </div>
-        <div className="right">
-          <img src={"/" + itemNum + ".jpg"} alt="item" />
-          <img
-            className="category"
-            src={`icon-${category}.jpg`}
-            alt="category"
-          />
-        </div>
-      </div>
-    </div>
-  );
+		return <svg ref={svgRef}></svg>;
+	}
+	return (
+		<div className="Item">
+			<div className="header">
+				<div className="itemnum">{itemNum}</div>
+				<div className="kor-description">{kor_description}</div>
+			</div>
+			<div className="content">
+				<div className="left">
+					<div className="container-1">
+						<div className="description">
+							{brand}, {description}
+						</div>
+					</div>
+					<div className="container-2">
+						<div className="size">{size}</div>
+						<div className="flex-container">
+							<div className="uom">{uom}</div>
+							<div className="exd">{exd}</div>
+							<div className="priceCode">{priceCode}</div>
+						</div>
+						<div className="barcodeRetail">{barcodeRetail}</div>
+					</div>
+					<div className="container-3">
+						<div className="barcodeNotRetail">{barcodeNotRetail}</div>
+						<Barcode value={barcodeRetail}></Barcode>
+					</div>
+				</div>
+				<div className="right">
+					<img src={"/" + itemNum + ".jpg"} alt="item" />
+					<img className="category" src={`icon-${category}.jpg`} alt="category" />
+				</div>
+			</div>
+		</div>
+	);
 }
