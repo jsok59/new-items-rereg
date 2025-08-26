bb3f104b0a9ddce0922f26afd1a912af54a18744
John <jsok@rheebros.com>
2025-08-26 08:31:40 -0400

    start localStorage feature

--- src/components/NewItems.jsx
+++ src/components/NewItems.jsx
@@ -5,38 +5,51 @@ import { useDataContext } from "./DataProvider.jsx";
 import NewItemModal from "./NewItemModal.jsx";
 
 export default function NewItems() {
-	const { itemData, barcodeData, loading, error } = useDataContext();
-	const { items, item, setItem, isOpen, openModal, closeModal, addItem, removeItem, editItem } = useItemManagement();
+  const { itemData, barcodeData, loading, error } = useDataContext();
+  const {
+    items,
+    item,
+    setItem,
+    isOpen,
+    openModal,
+    closeModal,
+    addItem,
+    removeItem,
+    editItem,
+  } = useItemManagement();
 
-	// Handle loading and error states
-	if (loading) return <div>Loading data...</div>;
-	if (error) return <div>Error loading data: {error.message}</div>;
+  // Handle loading and error states
+  if (loading) return <div>Loading data...</div>;
+  if (error) return <div>Error loading data: {error.message}</div>;
 
-	return (
-		<div className="Items">
-			<div>
-				<h3>New Items</h3>
-				<button onClick={openModal}>Add Item</button>
-			</div>
-			{isOpen === true && (
-				<NewItemModal
-					item={item}
-					setItem={setItem}
-					closeModal={closeModal}
-					itemData={itemData}
-					barcodeData={barcodeData}
-					addItem={addItem}
-				></NewItemModal>
-			)}
+  return (
+    <div className="Items">
+      <div>
+        <h3>New Items</h3>
+        <button onClick={openModal}>Add Item</button>
+        <button>Print</button>
+      </div>
+      {isOpen === true && (
+        <NewItemModal
+          item={item}
+          setItem={setItem}
+          closeModal={closeModal}
+          itemData={itemData}
+          barcodeData={barcodeData}
+          addItem={addItem}
+        ></NewItemModal>
+      )}
 
-			<ul>
-				{items.map((item) => (
-					<li key={item.id} className="item-container" id={item.id}>
-						<Item key={item.id} item={item} editItem={editItem}></Item>
-						<button onClick={(e) => removeItem(e.target.parentElement.id)}>Delete</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+      <ul>
+        {items.map((item) => (
+          <li key={item.id} className="item-container" id={item.id}>
+            <Item key={item.id} item={item} editItem={editItem}></Item>
+            <button onClick={(e) => removeItem(e.target.parentElement.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/components/ReregistrationItems.jsx
+++ src/components/ReregistrationItems.jsx
@@ -5,38 +5,51 @@ import { useDataContext } from "./DataProvider.jsx";
 import ReregistrationModal from "./ReregistrationModal.jsx";
 
 export default function ReregistrationItems() {
-	const { itemData, barcodeData, loading, error } = useDataContext();
-	const { items, item, setItem, isOpen, openModal, closeModal, addItem, removeItem, editItem } = useItemManagement();
+  const { itemData, barcodeData, loading, error } = useDataContext();
+  const {
+    items,
+    item,
+    setItem,
+    isOpen,
+    openModal,
+    closeModal,
+    addItem,
+    removeItem,
+    editItem,
+  } = useItemManagement();
 
-	// Handle loading and error states
-	if (loading) return <div>Loading data...</div>;
-	if (error) return <div>Error loading data: {error.message}</div>;
+  // Handle loading and error states
+  if (loading) return <div>Loading data...</div>;
+  if (error) return <div>Error loading data: {error.message}</div>;
 
-	return (
-		<div className="Items">
-			<div>
-				<h3>Reregistration Items</h3>
-				<button onClick={openModal}>Add Item</button>
-			</div>
-			{isOpen === true && (
-				<ReregistrationModal
-					item={item}
-					setItem={setItem}
-					closeModal={closeModal}
-					itemData={itemData}
-					barcodeData={barcodeData}
-					addItem={addItem}
-				></ReregistrationModal>
-			)}
+  return (
+    <div className="Items">
+      <div>
+        <h3>Reregistration Items</h3>
+        <button onClick={openModal}>Add Item</button>
+        <button>Print</button>
+      </div>
+      {isOpen === true && (
+        <ReregistrationModal
+          item={item}
+          setItem={setItem}
+          closeModal={closeModal}
+          itemData={itemData}
+          barcodeData={barcodeData}
+          addItem={addItem}
+        ></ReregistrationModal>
+      )}
 
-			<ul>
-				{items.map((item) => (
-					<li key={item.id} className="item-container" id={item.id}>
-						<Item key={item.id} item={item} editItem={editItem}></Item>
-						<button onClick={(e) => removeItem(e.target.parentElement.id)}>Delete</button>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+      <ul>
+        {items.map((item) => (
+          <li key={item.id} className="item-container" id={item.id}>
+            <Item key={item.id} item={item} editItem={editItem}></Item>
+            <button onClick={(e) => removeItem(e.target.parentElement.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
